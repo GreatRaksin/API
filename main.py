5789bbef2e1db50caaae6263cd6f5a66b06d8e43
demidraksin <greatraksin@icloud.com>
2019-11-26 13:39:05 +0300

    adding colorama

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ from colorama import init, Fore, Back, Style
 from google_currency import convert
 init()
 
-conv = input('Хотите конвертировать валюту? (да или нет) ').lower()
+conv = input(Fore.BLUE + 'Хотите конвертировать валюту? (да или нет) ' + Style.RESET_ALL).lower()
 
 while conv.startswith('д') or conv.startswith('l'):
-    amount = input('Сколько денег будем переводить? ')
+    amount = input(Fore.BLUE + 'Сколько денег будем переводить? ' + Style.RESET_ALL)
     try:
         amount = float(amount)
     except:
-        print('Пожалуйста, введите число!')
-        conv = input('Хотите конвертировать валюту? (да или нет) ').lower()
+        print(Fore.RED + 'Пожалуйста, введите число!' + Style.RESET_ALL)
+        conv = input(Fore.BLUE + 'Хотите конвертировать валюту? (да или нет) ' + Style.RESET_ALL).lower()
 
-    first = input('Из какой валюты будем переводить? ').upper()
-    second = input('В какую валюту будем переводить? ').upper()
+    first = input(Fore.LIGHTYELLOW_EX + 'Из какой валюты будем переводить? ').upper()
+    second = input('В какую валюту будем переводить? ' + Style.RESET_ALL).upper()
 
-    print(convert(first, second, amount))
+    result = convert(first, second, amount)
+    print(Fore.GREEN + result)
 
 
